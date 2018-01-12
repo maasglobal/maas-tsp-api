47ec4705e8428c971e3f6acbeef11f7dc3ab2075
Brylie Christopher Oxley <brylie.oxley@maas.global>
2018-01-12 11:23:45 +0200

    Add 'deploy' task with sub-tasks

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ const replace = require('gulp-replace');
 const yaml = require('gulp-yaml');
 const del = require('del');
 const connect = require('gulp-connect');
+const shell = require('gulp-shell')
 
 const swaggerRoot = 'specs/maas-v1.json'
 
@@ -40,9 +41,49 @@ gulp.task('serve', function() {
   });
 });
 
+gulp.task(
+  'create-temp-gh-pages-deployment-branch',
+  shell.task(
+    [
+      'git checkout -b gh-pages-tmp',
+      'rm .gitignore'
+    ]
+  )
+)
+
+gulp.task(
+  'deploy-temp-gh-pages-deployment-branch',
+  shell.task(
+    [
+      'git checkout -b gh-pages-tmp',
+      'rm .gitignore',
+      'git add .',
+      'git commit -am "Remove gitignore; add built docs"',
+      // Push the docs folder to the Github Pages branch
+      'git subtree push --prefix docs upstream gh-pages',
+    ]
+  )
+)
+
+gulp.task(
+  'delete-temp-gh-pages-deployment-branch',
+  shell.task(
+    [
+      // TODO: change the following line to 'git checkout master' after test
+      'git checkout gh-pages-deploy-script',
+      'git -d gh-pages-tmp'
+    ]
+  )
+)
+
 // Aliases
 
 gulp.task('docs', ['copy-swagger-ui', 'copy-specs']);
 gulp.task('build', ['transform-yaml']);
 gulp.task('clean', ['clean:docs']);
 gulp.task('watch', ['watch-specs']);
+gulp.task('deploy' [
+  'create-temp-gh-pages-deployment-branch',
+  'deploy-temp-gh-pages-deployment-branch',
+  'delete-temp-gh-pages-deployment-branch',
+])
