1f5eaf256ac0a913d5773df1fa542c70e3be5805
Ruslan Gainutdinov <ruslan.gainutdinov@maas.global>
2019-11-08 16:14:06 +0200

    Added example testing

---
+++ test/examples.spec.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const schemas = require('maas-schemas');
+
+beforeEach(() => {
+  schemas.init();
+});
+
+describe('Check examples', () => {
+  describe('taxi', () => {
+    it('booking-create-request.json', () => {
+      return schemas.validate(
+        require('../schemas/tsp/booking-create/request.json'),
+        require('../examples/taxi/booking-create-request.json')
+      );
+    });
+
+    it('booking-create-response.json', () => {
+      return schemas.validate(
+        require('../schemas/tsp/booking-create/request.json'),
+        require('../examples/taxi/booking-create-response.json')
+      );
+    });
+
+    it('booking-options-response.json', () => {
+      return schemas.validate(
+        require('../schemas/tsp/booking-options-list/response.json'),
+        require('../examples/taxi/booking-options-response.json')
+      );
+    });
+  });
+});
