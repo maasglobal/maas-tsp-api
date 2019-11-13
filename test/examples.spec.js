ca33516276423a454ef31c195eba44be1273fc3d
Ruslan Gainutdinov <ruslan.gainutdinov@maas.global>
2019-11-13 16:31:35 +0200

    Dont use require as it cached by mocha -w

--- test/examples.spec.js
+++ test/examples.spec.js
@@ -1,21 +1,20 @@
 'use strict';
 
-const schemas = require('maas-schemas');
-
 const BookingCreateRequest = require('maas-schemas-ts/lib/tsp/booking-create/request').Default;
 const BookingCreateResponse = require('maas-schemas-ts/lib/tsp/booking-create/response').Default;
 const BookingOptionsResponse = require('maas-schemas-ts/lib/tsp/booking-options-list/response').Default;
 const typePromise = require('io-ts-promise');
 const assert = require('assert');
 
-beforeEach(() => {
-  schemas.init();
-});
+const fs = require('fs');
 
 describe('Check examples', () => {
   describe('taxi', () => {
     it('booking-create-request.json', () => {
-      return typePromise.decode(BookingCreateRequest, require('../examples/taxi/booking-create-request.json'));
+      return typePromise.decode(
+        BookingCreateRequest,
+        JSON.parse(fs.readFileSync('./examples/taxi/booking-create-request.json'))
+      );
     });
 
     it('should fail decode', async () => {
@@ -31,11 +30,17 @@ describe('Check examples', () => {
     });
 
     it('booking-create-response.json', () => {
-      return typePromise.decode(BookingCreateResponse, require('../examples/taxi/booking-create-response.json'));
+      return typePromise.decode(
+        BookingCreateResponse,
+        JSON.parse(fs.readFileSync('./examples/taxi/booking-create-response.json'))
+      );
     });
 
     it('booking-options-response.json', () => {
-      return typePromise.decode(BookingOptionsResponse, require('../examples/taxi/booking-options-response.json'));
+      return typePromise.decode(
+        BookingOptionsResponse,
+        JSON.parse(fs.readFileSync('./examples/taxi/booking-options-response.json'))
+      );
     });
   });
 });
