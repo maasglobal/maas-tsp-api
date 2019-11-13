05d7987d016132160f6151f79a0a029166e617fa
Ruslan Gainutdinov <ruslan.gainutdinov@maas.global>
2019-11-13 14:54:05 +0200

    Validate via maas-schemas-ts

--- test/examples.spec.js
+++ test/examples.spec.js
@@ -2,6 +2,11 @@
 
 const schemas = require('maas-schemas');
 
+const BookingCreateRequestType = require("maas-schemas-ts/lib/tsp/booking-create/request")
+const BookingCreateResponseType = require("maas-schemas-ts/lib/tsp/booking-create/response")
+const BookingOptionsResponseType = require("maas-schemas-ts/lib/tsp/booking-options-list/response")
+const typePromise = require("io-ts-promise")
+
 beforeEach(() => {
   schemas.init();
 });
@@ -9,24 +14,18 @@ beforeEach(() => {
 describe('Check examples', () => {
   describe('taxi', () => {
     it('booking-create-request.json', () => {
-      return schemas.validate(
-        require('../schemas/tsp/booking-create/request.json'),
-        require('../examples/taxi/booking-create-request.json')
-      );
+      return typePromise.decode(BookingCreateRequestType,
+        require('../examples/taxi/booking-create-request.json'))
     });
 
     it('booking-create-response.json', () => {
-      return schemas.validate(
-        require('../schemas/tsp/booking-create/request.json'),
-        require('../examples/taxi/booking-create-response.json')
-      );
+      return typePromise.decode(BookingCreateRequestType,
+        require('../examples/taxi/booking-create-response.json'))
     });
 
     it('booking-options-response.json', () => {
-      return schemas.validate(
-        require('../schemas/tsp/booking-options-list/response.json'),
-        require('../examples/taxi/booking-options-response.json')
-      );
+      return typePromise.decode(BookingOptionsResponseType,
+        require('../examples/taxi/booking-options-response.json'))
     });
   });
 });
