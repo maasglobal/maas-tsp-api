a44d86bf63963cd627de79a0ca807135577b2e44
Ruslan Gainutdinov <ruslan.gainutdinov@maas.global>
2020-01-02 14:37:47 +0200

    Scooter examples

--- test/examples.spec.js
+++ test/examples.spec.js
@@ -3,8 +3,10 @@
 const BookingCreateRequest = require('maas-schemas-ts/lib/tsp/booking-create/request').Default;
 const BookingCreateResponse = require('maas-schemas-ts/lib/tsp/booking-create/response').Default;
 const BookingUpdateRequest = require('maas-schemas-ts/lib/tsp/booking-update/request').Default;
+const BookingUpdateResponse = require('maas-schemas-ts/lib/tsp/booking-update/response').Default;
 const BookingOptionsResponse = require('maas-schemas-ts/lib/tsp/booking-options-list/response').Default;
 const BookingReadByIdResponse = require('maas-schemas-ts/lib/tsp/booking-read-by-id/response').Default;
+const BookingReceiptResponse = require('maas-schemas-ts/lib/tsp/booking-receipt/response').Default;
 const typePromise = require('io-ts-promise');
 const assert = require('assert');
 
@@ -72,6 +74,57 @@ describe('Check examples', () => {
       return typePromise.decode(
         BookingUpdateRequest,
         JSON.parse(fs.readFileSync('./examples/car/booking-update-request-lock.json'))
+      );
+    });
+  });
+
+  describe('scooter', () => {
+    it('booking-create-request.json', () => {
+      return typePromise.decode(
+        BookingCreateRequest,
+        JSON.parse(fs.readFileSync('./examples/scooter/booking-create-request.json'))
+      );
+    });
+
+    it('booking-create-response.json', () => {
+      return typePromise.decode(
+        BookingCreateResponse,
+        JSON.parse(fs.readFileSync('./examples/scooter/booking-create-response.json'))
+      );
+    });
+
+    it('booking-options-response.json', () => {
+      return typePromise.decode(
+        BookingOptionsResponse,
+        JSON.parse(fs.readFileSync('./examples/scooter/booking-options-response.json'))
+      );
+    });
+
+    it('booking-read-by-id-response-activated.json', () => {
+      return typePromise.decode(
+        BookingReadByIdResponse,
+        JSON.parse(fs.readFileSync('./examples/scooter/booking-read-by-id-response-activated.json'))
+      );
+    });
+
+    it('booking-receipt-response.json', () => {
+      return typePromise.decode(
+        BookingReceiptResponse,
+        JSON.parse(fs.readFileSync('./examples/scooter/booking-receipt-response.json'))
+      );
+    });
+
+    it('booking-update-expired-request.json', () => {
+      return typePromise.decode(
+        BookingUpdateRequest,
+        JSON.parse(fs.readFileSync('./examples/scooter/booking-update-expired-request.json'))
+      );
+    });
+
+    it('booking-update-expired-response.json', () => {
+      return typePromise.decode(
+        BookingUpdateResponse,
+        JSON.parse(fs.readFileSync('./examples/scooter/booking-update-expired-response.json'))
       );
     });
   });
