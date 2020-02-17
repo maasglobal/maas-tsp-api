da176206e2aa3c2902cb1c2fcaaa73e5281b23f7
Ruslan Gainutdinov <ruslan.gainutdinov@maas.global>
2020-02-17 23:15:18 +0200

    Also generate png files for MermaidJS diagrams

--- utils/compile-workflows.js
+++ utils/compile-workflows.js
@@ -6,6 +6,38 @@ const workflows = process.argv[2] || './specs/workflow';
 const out = process.argv[3] || './dist/specs/workflow';
 
 /**
+ * Process workflow into specified format file
+ *
+ * @param {string} file - Relative path to file
+ * @param {string} format - Format, i.e. svg or png
+ */
+async function compileDiagram(file, format) {
+  return new Promise(async (resolve, reject) => {
+    const result = file.replace('.mmd', '.' + format);
+    // eslint-disable-next-line no-console
+    console.warn(`Compiling ${file} into ${result}`);
+    const child = spawn('mmdc', ['-i', workflows + '/' + file, '-o', out + '/' + result, '--scale', '4'], {
+      timeout: 5000,
+    });
+
+    child.stderr.on('data', data => {
+      // eslint-disable-next-line no-console
+      console.warn(`${file}: ${data}`);
+    });
+
+    child.on('close', code => {
+      if (code !== 0) {
+        // eslint-disable-next-line no-console
+        console.warn(`${file}: child process exited with code ${code}`);
+        reject(code);
+      } else {
+        resolve(code);
+      }
+    });
+  });
+}
+
+/**
  * Process all workflows into svg files
  */
 async function compileWorkflows() {
@@ -15,19 +47,7 @@ async function compileWorkflows() {
   fs.readdir(workflows, (err, files) => {
     files.forEach(file => {
       if (file.endsWith('.mmd')) {
-        const svg = file.replace('.mmd', '.svg');
-        const child = spawn('mmdc', ['-i', workflows + '/' + file, '-o', out + '/' + svg]);
-        child.stderr.on('data', data => {
-          // eslint-disable-next-line no-console
-          console.warn(`${file}: ${data}`);
-        });
-
-        child.on('close', code => {
-          if (code !== 0) {
-            // eslint-disable-next-line no-console
-            console.warn(`${file}: child process exited with code ${code}`);
-          }
-        });
+        compileDiagram(file, 'svg').then(_ => compileDiagram(file, 'png'));
       }
     });
   });
