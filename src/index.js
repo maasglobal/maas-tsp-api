30128e4b3787fa985f52a8d58843a7a39cf5cf4c
Ruslan Gainutdinov <ruslan.gainutdinov@maas.global>
2020-02-19 16:42:44 +0200

    Using css and fixed static build

--- src/index.js
+++ src/index.js
@@ -7,8 +7,18 @@ const { loadAndBundleSpec } = require('redoc');
 const { compileWorkflows } = require('../utils/compile-workflows');
 const { compileDocs } = require('../utils/compile-md');
 
-app.use('/', express.static('docs'));
+/**
+ * Makes sender function to be used as middleware on app
+ *
+ * @param {string} file - Path to send
+ */
+function sender(file) {
+  return async (req, res) => res.sendFile(path.resolve(__dirname, '..', file));
+}
 
+app.use('/', express.static('docs'));
+app.use('/overview.html', sender('dist/overview.html'));
+app.use('/github-markdown.css', sender('node_modules/github-markdown-css/github-markdown.css'));
 app.use('/assets', express.static('assets'));
 app.use('/specs', express.static('specs'));
 app.get('/specs/booking.json', async (req, res) =>
@@ -18,9 +28,8 @@ app.use('/specs/workflow/', express.static('dist/specs/workflow'));
 app.use('/examples', express.static('examples'));
 app.use('/schemas', express.static('schemas'));
 app.use('/swagger-ui', express.static(path.resolve(__dirname, '../node_modules/swagger-ui-dist')));
-app.use('/redoc.standalone.js', async (req, res) =>
-  res.sendFile(path.resolve(__dirname, '../node_modules/redoc/bundles/redoc.standalone.js'))
-);
+app.use('/redoc.standalone.js', sender('node_modules/redoc/bundles/redoc.standalone.js'));
+
 compileWorkflows()
   .then(compileDocs)
   .then(_ => {
--- utils/compile-md.js
+++ utils/compile-md.js
@@ -4,9 +4,29 @@ const fs = require('fs');
 const docs = process.argv[2] || './docs';
 const out = process.argv[3] || './dist';
 const marked = require('marked');
+const renderer = new marked.Renderer();
 
 /**
- * Process all workflows into svg files
+ * Make first letter uppercase leaving other intact
+ *
+ * @param {string} s - String to capitalize
+ */
+function capitalize(s) {
+  if (typeof s !== 'string') return s;
+  return s.charAt(0).toUpperCase() + s.slice(1);
+}
+
+/**
+ * When running locally, localize markdown links to local files
+ *
+ * @param {string} md - markdown content
+ */
+function localize(md) {
+  return md.split('https://maasglobal.github.io/maas-tsp-api/').join('');
+}
+
+/**
+ * Process all markdown files into html files
  */
 async function compileDocs() {
   return new Promise((resolve, reject) => {
@@ -19,9 +39,37 @@ async function compileDocs() {
       files.forEach(file => {
         if (file.endsWith('.md')) {
           const htmlFile = file.replace('.md', '.html');
-          const html = marked(fs.readFileSync(docs + '/' + file, { encoding: 'utf8' }));
-          // eslint-disable-next-line no-console
-          console.info(`Compiling ${file}`);
+          const md = fs.readFileSync(docs + '/' + file, { encoding: 'utf8' });
+          const htmlFragment = marked(localize(md), { renderer });
+          const html = `<!doctype html>
+<html>
+<head>
+  <meta charset="utf-8"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1"/>
+  <title>${capitalize(file.replace('.md', ''))}</title>
+  <link rel="stylesheet" type="text/css" href="github-markdown.css"/>
+  <style type="text/css">
+    .markdown-body {
+      box-sizing: border-box;
+      min-width: 200px;
+      max-width: 980px;
+      margin: 0 auto;
+      padding: 45px;
+    }
+
+    @media (max-width: 767px) {
+      .markdown-body {
+        padding: 15px;
+      }
+    }
+  </style>
+</head>
+<body class="markdown-body">
+  ${htmlFragment}
+</body>
+</html>
+`;
+
           fs.writeFileSync(out + '/' + htmlFile, html);
           count++;
         }
